8a0d5160a6529b0e086d3cf402c593ee89d5e097
0kasama <okaajyad@gmail.com>
2024-03-09 02:54:27 +0700

    Initial Auth Middlewares

---
+++ middlewares/auth-mid.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+
+const verify = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split("")[1];
+
+    jwt.verify(token, "secretKey", (err, decoded) => {
+      if (err) {
+        res.status(401).json({ message: "Unauthorized!" });
+      } else {
+        req.userLogged = { email: decoded.email, role: decoded.role };
+        next();
+      }
+    });
+  } catch (err) {
+    res.status(401).json({ message: "No Token!" });
+  }
+};
+
+const authorized = (req, res, next) => {
+  const { role } = req.userLogged;
+  if (role === "Admin") {
+    next();
+  } else {
+    res
+      .status(403)
+      .json({
+        message: "Do not have permission!",
+      });
+  }
+};
+
+module.exports = authorized;
+module.exports = verify;
