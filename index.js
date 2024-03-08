5de944fe0197d6a7a907dff3fda7a066050c5bf2
0kasama <okaajyad@gmail.com>
2024-03-09 02:55:24 +0700

    Initial Swagger JSDoc

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const swaggerUi = require("swagger-ui-express");
-const specs = require("./swagger.js");
+const specs = require("./docs/swagger.js");
 const port = 3000;
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
